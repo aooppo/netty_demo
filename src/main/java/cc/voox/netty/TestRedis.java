f2eb3f41e8ef55c0480ffe295503ae03c9a33704
TJ <i@voox.cc>
2021-05-16 17:00:15 +0800

    add redis test by netty

--- src/main/java/cc/voox/netty/TestRedis.java
+++ src/main/java/cc/voox/netty/TestRedis.java
@@ -2,31 +2,33 @@ package cc.voox.netty;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
 import java.nio.charset.Charset;
 
 public class TestRedis {
     public static void main(String[] args) throws InterruptedException {
-        final byte[] LINE = {13, 10};
+        final byte[] LINE = {'\r','\n'};
         NioEventLoopGroup nioEventLoopGroup = new NioEventLoopGroup();
 
         try {
             Bootstrap bootstrap = new Bootstrap();
 
-            bootstrap.group(nioEventLoopGroup)
+            ChannelFuture future = bootstrap.group(nioEventLoopGroup)
                     .channel(NioSocketChannel.class)
                     .handler(new ChannelInitializer<SocketChannel>() {
 
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new LoggingHandler());
+                            ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
                             ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
 
                                 @Override
@@ -48,13 +50,21 @@ public class TestRedis {
                                     buf.writeBytes(LINE);
                                     buf.writeBytes("zhangsan".getBytes());
                                     buf.writeBytes(LINE);
-                                    ctx.writeAndFlush(buf);
+//                                    ctx.writeAndFlush(buf);
+                                    ch.writeAndFlush(buf);
                                 }
+
+
 
                                 @Override
                                 public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                    System.out.println("Server channelRead....");
+                                    System.out.println(ctx.channel().remoteAddress()+"->Server :"+msg.toString());
+                                    ctx.write("Server write"+msg);
+                                    ctx.flush();
                                     ByteBuf byteBuf = (ByteBuf) msg;
                                     System.out.println(byteBuf.toString(Charset.defaultCharset()));
+                                    super.channelRead(ctx, msg);
                                 }
                             });
 
@@ -62,11 +72,12 @@ public class TestRedis {
                     })
                     .connect("127.0.0.1", 6379)
                     .sync();
+            future.channel().close().sync();
 
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
-            nioEventLoopGroup.shutdownGracefully();
+//            nioEventLoopGroup.shutdownGracefully();
         }
 
     }
